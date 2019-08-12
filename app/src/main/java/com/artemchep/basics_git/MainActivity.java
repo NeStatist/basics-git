bb220e5c6bc72b5f8671c9582406d56ae87098d0
NeStatist <doroshkor@gmail.com>
2019-08-12 20:02:03 +0300

    add button

--- app/src/main/java/com/artemchep/basics_git/MainActivity.java
+++ app/src/main/java/com/artemchep/basics_git/MainActivity.java
@@ -4,11 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.artemchep.basics_git.ui.postadd.PostAddFragment;
+import com.artemchep.basics_git.ui.postlist.ListFragment;
+
+public class MainActivity extends AppCompatActivity implements AddData, OnOpen {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentLayout, new ListFragment(this))
+                .commit();
+    }
+
+    @Override
+    public void onAddDataPost() {
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentLayout, new ListFragment(this))
+                .commit();
+
+    }
+
+    @Override
+    public void onOpenFrag() {
+
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentLayout, new PostAddFragment(this))
+                .commit();
     }
 }
